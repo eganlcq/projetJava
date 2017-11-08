f6cb5e128ea12876fb37e39afe2ff1e9e4b90cbb
eganlcq <32932521+eganlcq@users.noreply.github.com>
2017-11-08 23:13:50 +0100

    Create FloorTile1.java

---
+++ FloorTile1.java
@@ -0,0 +1,15 @@
+package main.tiles;
+
+import main.gfx.Assets;
+
+/**
+ * Cette classe représente une des deux cases de sol
+ * @author Octikoros
+ *
+ */
+public class FloorTile1 extends Tile{
+
+	public FloorTile1(int id) {
+		super(Assets.tile1, id);
+	}
+}
