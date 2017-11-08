2dbbd681a895ff8282b5d2c93822e43297fe985a
eganlcq <32932521+eganlcq@users.noreply.github.com>
2017-11-08 23:13:28 +0100

    Create BackgroundTile.java

---
+++ BackgroundTile.java
@@ -0,0 +1,15 @@
+package main.tiles;
+
+import main.gfx.Assets;
+
+/**
+ * Cette classe représente une case de fond d'écran
+ * @author Octikoros
+ *
+ */
+public class BackgroundTile extends Tile{
+
+	public BackgroundTile(int id) {
+		super(Assets.background, id);
+	}
+}
