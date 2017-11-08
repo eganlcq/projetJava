3ad812e43cd580be5857dc2dfc2317c9b12915f9
eganlcq <32932521+eganlcq@users.noreply.github.com>
2017-11-08 23:14:10 +0100

    Create FlorrTile2.java

---
+++ FlorrTile2.java
@@ -0,0 +1,15 @@
+package main.tiles;
+
+import main.gfx.Assets;
+
+/**
+ * Cette classe représente une des deux case de sol
+ * @author Octikoros
+ *
+ */
+public class FloorTile2 extends Tile{
+
+	public FloorTile2(int id) {
+		super(Assets.tile2, id);
+	}
+}
