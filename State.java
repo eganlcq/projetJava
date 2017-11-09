940415fb855a1affaf372ac8e196bd44052f4fcb
eganlcq <32932521+eganlcq@users.noreply.github.com>
2017-11-09 21:23:10 +0100

    Update State.java

--- State.java
+++ State.java
@@ -2,7 +2,7 @@ package main.states;
 
 import java.awt.Graphics;
 
-import main.Game;
+import main.Handler;
 
 /**
  * Cette classe regroupe tous les états que peut prendre le programme
@@ -11,15 +11,16 @@ import main.Game;
  */
 public abstract class State {
 
-	// Déclaration des données de jeu (permet d'entrer dans le fil d'exécution courant du jeu)
-	protected Game game;
+	// Déclaration d'un handler comportant les données de jeu
+	//permet d'entrer dans le fil d'exécution courant du jeu
+	protected Handler handler;
 	
 	/**
 	 * Initialisation d'un nouvel état
 	 * @param game : les paramètres à prendre en compte
 	 */
-	public State(Game game) {
-		this.game = game;
+	public State(Handler handler) {
+		this.handler = handler;
 	}
 	
 	/**
