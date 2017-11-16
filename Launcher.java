5c51369c5cf4b1f701a042bc8ba39ef13b9a9156
eganlcq <32932521+eganlcq@users.noreply.github.com>
2017-11-16 21:38:56 +0100

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,28 @@
+package main;
+
+import control.GameController;
+import model.Game;
+import view.GameViewGUI;
+
+/**
+ * Cette classe permet de lancer le jeu
+ * @author Octikoros
+ *
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		// Création du modèle
+		Game model = new Game("Pète une case !", 800, 600);
+		model.init();
+		// Création des contrôleurs : un pour chaque vue
+		// Chaque contrôleur doit avoir une référence vers le modèle pour pouvoir le commander
+		GameController ctrlGUI = new GameController(model);
+		// Création des vues
+		// Chaque vue doit connaître son contrôleur et avoir une référence vers le modèle
+		GameViewGUI GUI = new GameViewGUI(model, ctrlGUI);
+		// On donne la référence à la vue pour chaque contrôleur
+		ctrlGUI.addView(GUI);
+	}
+	
+}
