73f1c4d87981571df978c05208708d4df3489df1
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2017-11-30 20:56:45 +0100

    Update MobConsole.java

--- MobConsole.java
+++ MobConsole.java
@@ -3,46 +3,45 @@ package main.console;
 import model.Game;
 
 /**
- * Cette classe représente un ennemi implémenté en console
+ * Cette classe reprÃ©sente un ennemi implÃ©mentÃ© en console
  * @author Octikoros
  *
  */
 public class MobConsole implements Runnable{
 
-	// Vérifie si le thread de l'ennemi est en train de tourner ou non
-	private boolean running = false;
-	// Le fil d'exécution de l'ennemi
-	private Thread thread;
-	// Représente la direction du déplacement de l'ennemi
+	// VÃ©rifie si le thread de l'ennemi est en train de tourner ou non
+	public boolean running = false;
+	// ReprÃ©sente la direction du dÃ©placement de l'ennemi
 	private String direction;
 	// La position horizontale de l'ennemi
 	public int x;
 	// La position verticale de l'ennemi
 	public int y;
-	// Le déplacement horizontal
+	// Le dÃ©placement horizontal
 	private int xMove;
-	// Le déplacement vertical
+	// Le dÃ©placement vertical
 	private int yMove;
 	// Le jeu dans lequel l'ennemi va bouger
 	private Game game;
 	
 	/**
 	 * Initialisation d'un nouvel ennemi en console
-	 * @param game : le jeu dans lequel il se déplacera
+	 * @param game : le jeu dans lequel il se dÃ©placera
 	 * @param x : position horizontale
 	 * @param y : position verticale
-	 * @param direction : la direction du déplacement
+	 * @param direction : la direction du dÃ©placement
 	 */
 	public MobConsole(Game game, int x, int y, String direction) {
 		this.game = game;
 		this.x = x;
 		this.y = y;
 		this.direction = direction;
-		start();
+		running = true;
+		new Thread(this).start();
 	}
 	
 	/**
-	 * Initialisation des données de déplacement de l'ennemi
+	 * Initialisation des donnÃ©es de dÃ©placement de l'ennemi
 	 */
 	public void init() {
 		if(direction == "up") yMove = -1;
@@ -52,7 +51,7 @@ public class MobConsole implements Runnable{
 	}
 	
 	/**
-	 * Vérifie si il y a une collision avec le joueur
+	 * VÃ©rifie si il y a une collision avec le joueur
 	 * @return true si oui, false si non
 	 */
 	public boolean collisionPlayer() {
@@ -60,7 +59,7 @@ public class MobConsole implements Runnable{
 	}
 	
 	/**
-	 * Permet à l'ennemi de s'exécuter
+	 * Permet Ã  l'ennemi de s'exÃ©cuter
 	 */
 	@Override
 	public void run() {
@@ -68,13 +67,13 @@ public class MobConsole implements Runnable{
 		// Le nombre de frames auxquelles le jeu tournera par seconde
 		int fps = 3;
 		// L'intervalle de temps qu'il y a entre chaque updates
-		// Utilisé en nanosecondes
+		// UtilisÃ© en nanosecondes
 		double timeUpdate = 1000000000 / fps;
 		// Contiendra le temps restant avant d'effectuer de nouveau une update
 		double delta = 0.0;
 		// Contiendra le current time de la machine
 		long now;
-		// Enregistre la dernière fois que l'on a demandé le current time de la machine
+		// Enregistre la derniÃ¨re fois que l'on a demandÃ© le current time de la machine
 		long lastTime = System.nanoTime();
 				
 		while(running) {
@@ -82,10 +81,10 @@ public class MobConsole implements Runnable{
 			now = System.nanoTime();
 			// Ajoute le temps restant avant de rappeler update() et render()
 			delta += (now - lastTime) / timeUpdate;
-			// Met à jour la dernière fois que l'on a demandé le current time de la machine
+			// Met Ã  jour la derniÃ¨re fois que l'on a demandÃ© le current time de la machine
 			lastTime = now;
 					
-			// Vérifie si update() et render() peuvent se lancer ou non
+			// VÃ©rifie si update() et render() peuvent se lancer ou non
 			// Cela permet de respecter les 60 updates par secondes convenus
 			if(delta >= 1) {
 				move();
@@ -93,11 +92,10 @@ public class MobConsole implements Runnable{
 				delta --;
 			}
 		}
-		stop();
 	}
 	
 	/**
-	 * Change les données de déplacement de l'ennemi
+	 * Change les donnÃ©es de dÃ©placement de l'ennemi
 	 */
 	public void move() {
 		game.getWorldConsole().getGrid()[x][y] = "_";
@@ -138,32 +136,11 @@ public class MobConsole implements Runnable{
 	}
 	
 	/**
-	 * Vérifie les collisions avec un mur
+	 * VÃ©rifie les collisions avec un mur
 	 * @return true si oui, false si non
 	 */
 	public boolean notWalkable() {
 		return (game.getWorldConsole().getGrid()[x][y].equals("x"));
-	}
-	
-	/**
-	 * Démarre le fil d'exécution de l'ennemi
-	 */
-	public synchronized void start() {
-		running = true;
-		thread = new Thread(this);
-		thread.start();
-	}
-	
-	/**
-	 * Arrête le fil d'exécution de l'ennemi
-	 */
-	public synchronized void stop() {
-		running = false;
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 
 	/**
@@ -197,7 +174,7 @@ public class MobConsole implements Runnable{
 	}
 
 	/**
-	 * @return la valeur du déplacement horizontal de l'ennemi
+	 * @return la valeur du dÃ©placement horizontal de l'ennemi
 	 */
 	public int getxMove() {
 		return xMove;
@@ -205,21 +182,21 @@ public class MobConsole implements Runnable{
 
 	/**
 	 * Change la valeur de xMove
-	 * @param xMove : le déplacement horizontal de l'ennemi
+	 * @param xMove : le dÃ©placement horizontal de l'ennemi
 	 */
 	public void setxMove(int xMove) {
 		this.xMove = xMove;
 	}
 
 	/**
-	 * @return la valeur du déplacement vertical de l'ennemi
+	 * @return la valeur du dÃ©placement vertical de l'ennemi
 	 */
 	public int getyMove() {
 		return yMove;
 	}
 	/**
 	 * Change la valeur de yMove
-	 * @param yMove : le déplacement vertical de l'ennemi
+	 * @param yMove : le dÃ©placement vertical de l'ennemi
 	 */
 	public void setyMove(int yMove) {
 		this.yMove = yMove;
@@ -228,3 +205,4 @@ public class MobConsole implements Runnable{
 	
 
 }
+
